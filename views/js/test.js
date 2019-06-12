00fcdc499225cc36dbb6924ecb4b630c18d7f2fd
Tin Huynh <trungtin2qn1@gmail.com>
2019-06-12 23:25:09 +0700

    Update

--- views/js/test.js
+++ views/js/test.js
@@ -1,5 +1,6 @@
-function TestgetAllBooks(input){
-    var res = getAllBooks(input)
-    console.log("res[0]:", res[0])
-    console.log("res[1]:", res[1])
+const TestgetAllBooks = async function(input){
+    var res = await getAllBooks(input);
+    console.log("res:", res);
+    console.log("res[0]:", res[0]);
+    console.log("res[1]:", res[1]);
 }
