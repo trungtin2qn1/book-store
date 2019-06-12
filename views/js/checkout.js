769f6227d9be0be21e04d1d7ec02872eee5a619d
Tin Huynh <trungtin2qn1@gmail.com>
2019-06-12 13:59:42 +0700

    Update

--- views/js/checkout.js
+++ views/js/checkout.js
@@ -22,13 +22,11 @@ function ready(){
 
 }
 
-
 function quantityChanged(event){
 	var input = event.target;
 	if(isNaN(input.value) || input.value <= 0){
 		input.value = 1;
 	}
-
 	updateCartTotal();
  }
 
