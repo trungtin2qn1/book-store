5cf2234c53928ebcc629c8d992b199f714701194
hongtoinguyen <nguyenhongtoi1@gmail.com>
2019-06-12 18:17:32 +0700

    update add-to-cart js

--- views/js/checkout.js
+++ views/js/checkout.js
@@ -20,6 +20,55 @@ function ready(){
 		input.addEventListener('change', quantityChanged)
 	}
 
+	var addItemToCartBtn = document.getElementsByClassName('btn-add-to-cart');
+	for (var i = 0; i < addItemToCartBtn.length ; i ++) {
+		var btn = addItemToCartBtn[i];
+		btn.addEventListener('click', addCartItemClicked);
+	} 
+
+}
+
+function addCartItemClicked(event){
+	var btnClicked = event.target;
+	var pItems = btnClicked.parentElement.parentElement;
+	var pro_title = pItems.getElementsByClassName('product-title')[0].innerText;
+	var pro_price = pItems.getElementsByClassName('product-price')[0].innerText;
+	var pro_img =  pItems.getElementsByClassName('product-image')[0].src;
+	console.log(pro_title, pro_price, pro_img);
+	addItemToCart(pro_title,pro_price, pro_img);
+}
+
+function addItemToCart(title,price,imgsrc){
+	var cartRow = document.createElement('tr');
+	// cartRow.classList.add('cart-row');
+	cartRow.innerText = title;
+	var cartItems = document.getElementsByClassName('cart-row').innerHTML;
+	console.log(cartItems);
+	// var cartRowContent =`
+	// 						<td data-th="Product">
+ //                                <div class="row">
+ //                                    <div class="col-sm-2 hidden-xs">
+ //                                        <img class="cart-product-image" src="${imgsrc}" alt="7-Day" class="img-responsive" />
+ //                                    </div>
+ //                                    <div class="col-sm-10">
+ //                                        <h4 class="cart-product-title">${title}</h4>
+ //                                        <p class="cart-product-des">Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+ //                                    </div>
+ //                                </div>
+ //                            </td>
+ //                            <td class="cart-price" data-th="Price">${price}</td>
+ //                            <td  data-th="Quantity">
+ //                              <input type="number" class="cart-quantity" value="1">
+ //                          </td>
+ //                          <td data-th="Subtotal" class="text-center">1.99</td>
+ //                          <td class="actions" data-th="">
+ //                              <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+ //                              <button class="btn gray-btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
+ //                          </td>
+	// `; 
+	// cartRow.innerHTML = cartRowContent;
+	// cartItems.append(cartRow);
+
 }
 
 function quantityChanged(event){
