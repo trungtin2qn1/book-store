e7328e589a3a397aff063ad97eb4e003de4f320e
hongtoinguyen <nguyenhongtoi1@gmail.com>
2019-06-17 18:29:23 +0700

    detail book function is not done yet

--- views/js/loadBook.js
+++ views/js/loadBook.js
@@ -1,5 +1,4 @@
-var count = 0;
-var div = document.getElementById('all-books');
+
 
 const loadBooks = async function () {
     const response = await fetch('http://127.0.0.1:2013/api/auth/customer/1/books');
@@ -7,22 +6,16 @@ const loadBooks = async function () {
     return myJson;
 }
 
-// const detailBook = async function(idBook){
-// 	const res = await fetch('http://127.0.0.1:2013/api/auth/customer/123/book/' + idBook'');
-// 	const detBook = await res.json();
-// 	return detBook;
-// }
-
-
-
 function load() {
+	var count = 0;
+	var div = document.getElementById('all-books');
     loadBooks()
         .then((books) => {
             for (var i = 0; i < books.length; i++) {
                 div.innerHTML += `
                     <div class="col-lg-2 col-md-3 col-sm-4">
                             <div class="item">
-                                <a href="product-single.html"><img class="product-image" src="${books[count].images}" alt="img">
+                                <a href="product-single.html" onclick="bd('${books[count].id}')"><img class="product-image" src="${books[count].images}" alt="img">
                                     <h3 class="product-title">${books[count].title}</h3>
                                 </a>
                                 <h6>Price: <span class="product-price">${books[count].price}</span></h6>
@@ -38,33 +31,25 @@ function load() {
         })
 }
 
+function bd(id){
+	setCache('id',id);
+	console.log('id: ',id);
+}
+
+function getCache(key) {
+    console.log(localStorage.getItem(key))
+    return localStorage.getItem(key)
+}
+function deleteCache(key) {
+    localStorage.removeItem(key)
+    return true
+}
+
+function setCache(key, value) {
+    localStorage.setItem(key, value)
+    return true
+}
+
 
 window.onload = load;
 
-// const loadMoreBooks = async function(){
-// 	let res = await fetch('http://127.0.0.1:2013/api/auth/customer/1/books');
-// 	let posts = await res.json();
-// 	return posts;
-// }
-// function loadBooks(){
-// 	loadMoreBooks()
-// 	.then((posts) => {
-// 		for (var i = 0; i < 10; i++) {
-// 			div.innerHTML +=`
-// 			<div class="col-lg-2 col-md-3 col-sm-4">
-//                     <div class="item">
-//                         <a href="product-single.html"><img class="product-image" src="${posts[count].images}" alt="img">
-//                             <h3 class="product-title">${posts[count].title}</h3>
-//                         </a>
-//                         <h6>Price: <span class="product-price">${posts[count].price}</span></h6>
-//                         <button class="btn btn-add-to-cart">Add to Cart</button>
-//                     </div>
-//                 </div>
-// 			`
-// 			count = count + 1;
-// 		}
-// 	})
-// 	.catch((error) => {
-// 		console.log(error);
-// 	})
-// }
